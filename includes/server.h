3433e4f96924ff64c7e770f669666214ffefcfa8
daie <elias.dai@etu.u-paris.fr>
2026-03-27 18:29:59 +0100

    Création du dossier src, du fichier du src avec la configuration de la socket et d'un header dédier qui contient que le numéro de port du server

---
+++ includes/server.h
@@ -0,0 +1,6 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#define PORT 4321
+
+#endif
---
+++ src/server.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/socket.h>
+
+#include "server.h"
+
+/**
+ * @brief Configures the server by creating a socket, 
+ * setting socket options, binding to a port, 
+ * and listening for incoming connections.
+ * 
+ * @return int The file descriptor for the server socket.
+ */
+static int server_config()
+{
+    // Create a socket for the server
+    int socket_fd = socket(AF_INET6, SOCK_STREAM, 0);
+
+    if(socket_fd == -1) {
+        perror("socket server");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set socket options to allow reuse of the address and port :
+    if(setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) == -1) {
+        perror("setsockopt server");
+        close(socket_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in6 server_addr;
+
+    memset(&server_addr, 0, sizeof(server_addr));
+
+    server_addr.sin6_family = AF_INET6;
+    server_addr.sin6_addr = in6addr_any;
+    server_addr.sin6_port = htons(PORT);
+
+    // Bind the socket to the specified port :
+    if(bind(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("bind server");
+        goto error;
+    }
+
+    // Listen for incoming connections :
+    if(listen(socket_fd, 0) == -1) {
+        perror("listen server");
+        goto error;
+    }
+
+    return socket_fd;
+
+    error:
+    close(socket_fd);
+    exit(EXIT_FAILURE);
+}
+
+int main() {
+
+    int socket_fd = server_config();
+    
+    short ret = EXIT_FAILURE;
+
+    
+    ret = EXIT_SUCCESS;
+
+    error:
+    close(socket_fd);
+
+    exit(ret);
+}
